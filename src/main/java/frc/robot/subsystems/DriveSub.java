34a1b7731b18e028de54c88b0ded375cd8ecdb74
Kevin Sinclair <ksinclair@sgvinternational.com>
2023-02-02 10:44:24 -0600

    Added TalonSRX into Drive Subsystem

--- src/main/java/frc/robot/subsystems/DriveSub.java
+++ src/main/java/frc/robot/subsystems/DriveSub.java
@@ -1,7 +1,23 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import srx
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX; 
+
 public class DriveSub extends SubsystemBase {
-    
+    private final TalonSRX  leftMotor= new TalonSRX(13);
+    private final TalonSRX  rightMotor= new TalonSRX(14);
+ 
+    public DriveSub() {
+       
+    }
+
+    public void arcadeDrive(double fwd, double rot){
+        leftMotor.set(ControlMode.PercentOutput,fwd);
+        rightMotor.set(ControlMode.PercentOutput, rot);
+    }
 }
